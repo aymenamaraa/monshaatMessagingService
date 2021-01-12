a6717aa03e1cb9688be858a55b6691bace599fb3
Aymen <aymenamaraa@gmail.com>
2021-01-12 09:10:19 +0100

    first commit

---
+++ config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  MONGO_URL: "mongodb://localhost:27017/chat",
+  PORT: 4000,
+  //   SECRET_KEY: "chatAppSecretKey",
+};
---
+++ index.js
@@ -0,0 +1,80 @@
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+app.use(bodyParser.json());
+const server = require("http").createServer(app);
+// const io = require("socket.io").listen(server);
+const io = require("socket.io")(server);
+const mongoose = require("mongoose");
+const config = require("./config");
+const cors = require("cors");
+const jwt = require("jsonwebtoken");
+
+const communityChat = require("./models/communityChat.model");
+const user = require("./models/chatUser.model");
+
+app.get("/oldmessages/:type/:correspondant", cors(), (req, res) => {
+  req.params.type === "community"
+    ? communityChat.find({}).exec((err, docs) => {
+        err ? res.end() : res.send(docs);
+      })
+    : res.send([]);
+});
+
+// app.options("/signin", cors());
+// app.post("/signin", cors(), (req, res) => {
+//   user
+//     .findOne({
+//       username: req.body.username,
+//       password: req.body.password,
+//     })
+//     .exec((err, doc) => {
+//       if (!doc) {
+//         res.status(401).send("Invalid credentials");
+//       } else {
+//         let token = jwt.sign(
+//           { userid: doc._id, username: doc.username },
+//           config.SECRET_KEY
+//         );
+//         res.send(token);
+//       }
+//     });
+// });
+
+app.get("/allusers", cors(), (req, res) => {
+  user.find({}).exec((err, docs) => {
+    err
+      ? res.end()
+      : res.send(docs.map((el) => ({ _id: el._id, username: el.username })));
+  });
+});
+
+io.on("connection", (socket) => {
+  console.log("user connected", socket.id);
+
+  socket.on("chat message", (msg) => {
+    console.log("msg", msg);
+    let chatMessage = new communityChat({
+      content: msg.content,
+      sender: msg.sender,
+      time: new Date(),
+    });
+    chatMessage.save();
+    io.emit("chat message", msg);
+  });
+});
+
+//save chat to the database
+mongoose
+  .connect(config.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    // useCreateIndex: true,
+  })
+  .then((db) => {
+    console.log("connected correctly to DB");
+  });
+
+server.listen(config.PORT, () =>
+  console.log("server running on port:" + config.PORT)
+);
